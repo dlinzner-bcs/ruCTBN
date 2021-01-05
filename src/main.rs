1cc56b4686c6eb30b4ca3ff354740d4bfbe2743c
dlinzner-bcs <dlinzner90@gmail.com>
2021-01-05 21:38:32 +0100

    05/01/2021 - starting ctbn sampler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+use ndarray::prelude::*;
+use ndarray::Array;
+use rand::distributions;
+
+struct CIM {
+    dims: int32,
+    pims: int32,
+    val : Array::<f64, _>::ones((dims, dims,pims))
+}
+
+fn create_CIM(dims: int32, pims: int32, alpha: f64, beta: f64) -> CIM {
+    let mut IM = Array::<f64, _>::ones((dims, dims));
+    for (i,j) in (5..10).enumerate(){
+            IM[[i,j]] = 1;
+    }
+
+    CIM {
+        dims: dims,
+        pims: pims,
+
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let a = arr2(&[[1.,2.,3.], [4.,5.,6.]]);
+    let mut b = Array::<f64, _>::ones((2, 3).f());
+
+    b[[0,0]]= b[[0,0]]+1.;
+
+    println!("{}",a+b);
 }
